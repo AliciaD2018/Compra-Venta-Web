6ce06633eaad5cfc07ff5bbadac6af8a751373a9
AliciaD2018 <aliciadiazrivas@gmail.com>
2021-06-14 04:35:59 -0600

    modifica data

--- src/Views/Shopping_history.js
+++ src/Views/Shopping_history.js
@@ -1,17 +1,54 @@
 import React, { Component } from 'react';
+import {Table} from 'react-bootstrap';
 
 class Shopping_history extends Component {
     constructor(props) {
         super(props);
-        this.value=""
+        this.state = {
+            nombre: '',
+            precio: '',
+            descripcion: '',
+            propietario: ''
+
+        }
     }
-    
+
     render() {
         return (
             <div>
-            <h1 class = "oblique" align='center'> Historial de Compras </h1> 
+                <h1 class="oblique" align='center'> Historial de Compras </h1>
+
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Username</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td colSpan="2">Larry the Bird</td>
+                            <td>@twitter</td>
+                        </tr>
+                    </tbody>
+                </Table>
             </div>
-            
+
         );
     }
 }
